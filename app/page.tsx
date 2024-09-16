7b7fe24250cbf13b18d54da4acdd84253798d4e6
Ejayz <32452006+Ejayz@users.noreply.github.com>
2024-09-17 07:52:46 +0800

    Init

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
+import IndexHeader from "@/components/UI/IndexHeader";
 import Image from "next/image";
-
 export default function Home() {
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+   <IndexHeader>
+     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -24,13 +26,14 @@ export default function Home() {
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
+          <a data-aos="fade-up"
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
           >
             <Image
+              
               className="dark:invert"
               src="https://nextjs.org/icons/vercel.svg"
               alt="Vercel logomark"
@@ -96,6 +99,77 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <li className="mb-2">
+          Get started by editing{" "}
+          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            app/page.tsx
+          </code>
+          .
+        </li>
+        <li>Save and see your changes instantly.</li>
+      </ol>
+      <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <li className="mb-2">
+          Get started by editing{" "}
+          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            app/page.tsx
+          </code>
+          .
+        </li>
+        <li>Save and see your changes instantly.</li>
+      </ol>
+      <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <li className="mb-2">
+          Get started by editing{" "}
+          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            app/page.tsx
+          </code>
+          .
+        </li>
+        <li>Save and see your changes instantly.</li>
+      </ol>
+      <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <li className="mb-2">
+          Get started by editing{" "}
+          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            app/page.tsx
+          </code>
+          .
+        </li>
+        <li>Save and see your changes instantly.</li>
+      </ol>
+      <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <li className="mb-2">
+          Get started by editing{" "}
+          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            app/page.tsx
+          </code>
+          .
+        </li>
+        <li>Save and see your changes instantly.</li>
+      </ol>
+      <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <li className="mb-2">
+          Get started by editing{" "}
+          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            app/page.tsx
+          </code>
+          .
+        </li>
+        <li>Save and see your changes instantly.</li>
+      </ol>
+      <ol data-aos="fade-up" className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+        <li className="mb-2">
+          Get started by editing{" "}
+          <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
+            app/page.tsx
+          </code>
+          .
+        </li>
+        <li >Save and see your changes instantly.</li>
+      </ol>
     </div>
+   </IndexHeader>
   );
 }
