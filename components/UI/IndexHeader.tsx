78e60c8ccb8ce94f3cd6d315aa005c5ab53573e3
Ejayz <32452006+Ejayz@users.noreply.github.com>
2024-09-18 23:54:59 +0800

    Update

--- components/UI/IndexHeader.tsx
+++ components/UI/IndexHeader.tsx
@@ -6,6 +6,8 @@ import "aos/dist/aos.css";
 import Link from "next/link";
 import Image from "next/image";
 import Footer from "./Footer";
+import { Toaster } from "react-hot-toast";
+import { QueryClient, QueryClientProvider, useQueryClient } from "@tanstack/react-query";
 export default function IndexHeader({
   children,
 }: Readonly<{
@@ -14,97 +16,101 @@ export default function IndexHeader({
   useEffect(() => {
     window.document ? Aos.init() : null;
   }, []);
-
+  const queryClient = new QueryClient();
   return (
-    <div className="drawer">
-      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-      <div className="drawer-content flex flex-col">
-        {/* Navbar */}
-        <div
-          data-aot={"zoom-out"}
-          data-aos-anchor-placement="center-center"
-          className="navbar bg-white w-full shadow-md  glass"
-        >
-          <div className="flex-none lg:hidden">
-            <label
-              htmlFor="my-drawer-3"
-              aria-label="open sidebar"
-              className="btn btn-square btn-ghost"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                className="inline-block h-6 w-6 stroke-current"
+    <QueryClientProvider client={queryClient}>
+      <div className="drawer">
+        <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col">
+          {/* Navbar */}
+          <div
+            data-aot={"zoom-out"}
+            data-aos-anchor-placement="center-center"
+            className="navbar bg-white w-full shadow-md  glass"
+          >
+            <div className="flex-none lg:hidden">
+              <label
+                htmlFor="my-drawer-3"
+                aria-label="open sidebar"
+                className="btn btn-square btn-ghost"
               >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h16M4 18h16"
-                ></path>
-              </svg>
-            </label>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block h-6 w-6 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4 6h16M4 12h16M4 18h16"
+                  ></path>
+                </svg>
+              </label>
+            </div>
+            <div className="mx-2 flex-1 px-2">
+              <Image
+                src="/img/logo/aqm_logo_double.png"
+                className=""
+                alt="logo"
+                width={200}
+                height={100}
+              />
+            </div>
+            <div className="hidden flex-none lg:block">
+              <ul className="menu menu-horizontal text-base">
+                {/* Navbar menu content here */}
+                <li>
+                  <Link href="/"> Home </Link>
+                </li>
+                <li>
+                  <Link href="/about"> About </Link>
+                </li>
+                <li>
+                  <Link href="/contact"> Contact </Link>
+                </li>
+                <li>
+                  <Link href="/services"> Services </Link>
+                </li>
+                <li>
+                  <Link href="/productgallery"> Product Gallery </Link>
+                </li>
+              </ul>
+            </div>
           </div>
-          <div className="mx-2 flex-1 px-2">
-            <Image
-              src="/img/logo/aqm_logo_double.png"
-              className=""
-              alt="logo"
-              width={200}
-              height={100}
-            />
-          </div>
-          <div className="hidden flex-none lg:block">
-            <ul className="menu menu-horizontal text-base">
-              {/* Navbar menu content here */}
-              <li>
-                <Link href="/"> Home </Link>
-              </li>
-              <li>
-                <Link href="/about"> About </Link>
-              </li>
-              <li>
-                <Link href="/contact"> Contact </Link>
-              </li>
-              <li>
-                <Link href="/services"> Services </Link>
-              </li>
-              <li>
-                <Link href="/productgallery"> Product Gallery </Link>
-              </li>
-            </ul>
-          </div>
+          <Toaster position="top-right" reverseOrder={false} />
+
+          {/* Page content here */}
+          {children}
+          <Footer />
         </div>
-        {/* Page content here */}
-        {children}
-        <Footer />
+        <div className="drawer-side">
+          <label
+            htmlFor="my-drawer-3"
+            aria-label="close sidebar"
+            className="drawer-overlay"
+          ></label>
+          <ul className="menu bg-base-200 min-h-full w-80 p-4">
+            {/* Sidebar content here */}
+            <li>
+              <Link href="/"> Home </Link>
+            </li>
+            <li>
+              <Link href="/about"> About </Link>
+            </li>
+            <li>
+              <Link href="/contact"> Contact </Link>
+            </li>
+            <li>
+              <Link href="/services"> Services </Link>
+            </li>
+            <li>
+              <Link href="/productgallery"> Product Gallery </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="drawer-side">
-        <label
-          htmlFor="my-drawer-3"
-          aria-label="close sidebar"
-          className="drawer-overlay"
-        ></label>
-        <ul className="menu bg-base-200 min-h-full w-80 p-4">
-          {/* Sidebar content here */}
-          <li>
-            <Link href="/"> Home </Link>
-          </li>
-          <li>
-            <Link href="/about"> About </Link>
-          </li>
-          <li>
-            <Link href="/contact"> Contact </Link>
-          </li>
-          <li>
-            <Link href="/services"> Services </Link>
-          </li>
-          <li>
-            <Link href="/productgallery"> Product Gallery </Link>
-          </li>
-        </ul>
-      </div>
-    </div>
+    </QueryClientProvider>
   );
 }
---
+++ components/VIEW/ContactView.tsx
@@ -0,0 +1,150 @@
+"use client";
+
+import { Form, Formik } from "formik";
+import { FormInput, FormSelect, FormTextArea } from "../UI/FormInput";
+import * as Yup from "yup";
+import { useMutation } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+export default function ContactView() {
+  const contactFormValidation = Yup.object().shape({
+    name: Yup.string().required("Name is required"),
+    email: Yup.string().email().required("Email is required"),
+    phone: Yup.string(),
+    message: Yup.string().required("Message is required"),
+    subject: Yup.string().required("Subject is required"),
+  });
+
+  const mutateContactForm = useMutation({
+    mutationFn: async (values: any) => {
+      const response = await fetch("/api/public/contact_form", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      return response.json();
+    },
+    onError: (error) => {
+      toast.error("Message failed to send.Please try again later.");
+    },
+    onSuccess: (data) => {
+      toast.success("Message sent successfully. We will get back to you soon.");
+    },
+  });
+
+  return (
+    <>
+      <div className="w-full h-screen flex min-h-screen ">
+        <div className="mx-auto my-auto w-3/4 lg:w-7/12 h-auto flex flex-col-reverse   lg:flex-row">
+          <div className="bg-base-100 lg:w-5/12 w-full rounded-tl-md rounded-bl-md shrink-0 shadow-2xl">
+            <Formik
+              initialValues={{
+                name: "",
+                email: "",
+                phone: "",
+                message: "",
+                subject: "",
+              }}
+              validationSchema={contactFormValidation}
+              onSubmit={(values, actions) => {
+                console.log(values)
+                mutateContactForm.mutate(values);
+              }}
+            >
+              {({ errors, touched }: any) => (
+                <Form className="p-12 gap-4 flex flex-col">
+                  <h1 className="text-2xl font-bold">Contact Form</h1>
+                  <FormInput
+                    errors={errors.name}
+                    touched={touched.name}
+                    tooltip="Enter your full name"
+                    name="name"
+                    placeholder="John Doe"
+                    label="Full Name"
+                  />
+                  <FormInput
+                    errors={errors.email}
+                    touched={touched.email}
+                    tooltip="Enter your email address"
+                    name="email"
+                    placeholder="Email"
+                    label="Email"
+                  />
+                  <FormInput
+                    errors={errors.phone}
+                    touched={touched.phone}
+                    tooltip="Enter your phone number"
+                    name="phone"
+                    placeholder="Phone"
+                    label="Phone"
+                  />
+                  <FormSelect
+                    errors={errors.subject}
+                    touched={touched.subject}
+                    tooltip="Select your subject"
+                    name="subject"
+                    label="Subject"
+                    options={[
+                      { value: "General Inquiry", label: "General Inquiry" },
+                      { value: "Sales", label: "Sales" },
+                      { value: "Support", label: "Support" },
+                      { value: "Feedback", label: "Feedback" },
+                      { value: "Other", label: "Other" },
+                    ]}
+                    placeholder="Subject"
+                  />
+
+                  <FormTextArea
+                    errors={errors.message}
+                    touched={touched.message}
+                    tooltip="Enter your message"
+                    name="message"
+                    placeholder="Message"
+                    label="Message"
+                  />
+                  <div className="lg:mx-auto w-3/4">
+                    <button
+                      type="submit"
+                      className={`btn w-full ${
+                        mutateContactForm.isPending
+                          ? "btn-disabled"
+                          : "btn-primary"
+                      }`}
+                    >
+                      {mutateContactForm.isPending ? (
+                        <div className="flex justify-center items-center">
+                          <div className="animate-spin rounded-full h-5 w-5 border-b-2 border-white"></div>{" "}
+                          Sending ...
+                        </div>
+                      ) : (
+                        "Submit"
+                      )}
+                    </button>
+                  </div>
+                </Form>
+              )}
+            </Formik>
+          </div>
+          <div
+            className=" flex flex-col lg:w-3/4 w-full text-center  "
+            style={{
+              backgroundImage: `url(/img/assets/contact_us_customer.webp)`,
+              backgroundSize: "cover",
+              backgroundPosition: "center",
+            }}
+          >
+            <div className="bg-base-100 bg-opacity-30 flex flex-col p-12 w-full h-full">
+              <h1 className="text-5xl mt-auto font-bold">Contact Us</h1>
+              <p className="text-lg">
+                If you have any questions or concerns, please feel free to
+                contact us.
+              </p>
+              <p className="text-lg mb-auto">We are here to help you.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
